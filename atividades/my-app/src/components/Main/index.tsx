a03aeb5981d96e040a0cb51bce9086b9906f3052
Caique Moura <caique.fred@outlook.com>
2024-05-27 18:45:22 -0300

    att atividades

---
+++ atividades/my-app/src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+export default function Main(){
+    return <WrapperSld>teste</WrapperSld>
+}
+
+const WrapperSld = styled.div`
+    display: flex;
+    border: 1px solid #888;
+    border-radius: 5px;
+    padding: 3px;
+`
